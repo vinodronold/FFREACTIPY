29345df696352e3a44c5485d41b04ec0dfdeaa42
Vinod Ronold <vinodronoldbe@gmail.com>
2017-05-23 16:25:27 -0400

    Logo Update

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,29 +2,32 @@ import React from 'react'
 
 export default () => (
   <svg
-    height="55"
+    height="110"
     version="1.1"
-    width="110"
-    style={{ overflow: 'hidden', position: 'relative', top: '-0.2px', height: '100%', width: '100%' }}>
+    width="220"
+    style={{
+      overflow: 'hidden',
+      position: 'relative',
+      margin: 'auto',
+    }}>
     <defs />
-    <rect x="5" y="5" width="100" height="25" rx="2" ry="2" fill="none" stroke="#ffffff" />
+    <rect x="10" y="10" width="200" height="50" rx="4" ry="4" fill="none" stroke="#ffffff" />
     <path
       fill="none"
       stroke="#ffffff"
-      d="M5,10L105,10M5,15L105,15M5,20L105,20M5,25L105,25M27.82,5L27.82,30M46.57,5L46.57,30M66.57,5L66.57,30M87.66,5L87.66,30"
+      d="M10,20L210,20M10,30L210,30M10,40L210,40M10,50L210,50M55.64,10L55.64,60M93.14,10L93.14,60M133.14,10L133.14,60M175.32,10L175.32,60"
     />
-    <circle cx="56.57" cy="17.5" r="0.75" fill="none" stroke="#ffffff" />
-    <circle cx="95.91" cy="17.5" r="0.75" fill="none" stroke="#ffffff" />
+    <circle cx="113.14" cy="35" r="1.5" fill="#ffffff" stroke="#ffffff" />
+    <circle cx="191.82" cy="35" r="1.5" fill="#ffffff" stroke="#ffffff" />
     <text
-      style={{ textAnchor: 'middle', fontFamily: '&quot;Arial&quot;', fontSize: '16px', strokeOpacity: 0 }}
-      x="55"
-      y="40"
+      x="110"
+      y="80"
       textAnchor="middle"
       fontFamily="&quot;Arial&quot;"
-      fontSize="16px"
+      fontSize="24px"
       stroke="#ffffff"
       fill="#ffffff"
-      strokeWidth="0.5"
+      strokeWidth="1"
       strokeOpacity="0">
       <tspan dy="5.5">f i v e f r e t s</tspan>
     </text>

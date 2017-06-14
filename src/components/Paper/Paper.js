404c40a73dc44ad4553f99e1326e235455160d60
Vinod Ronold <vinodronoldbe@gmail.com>
2017-06-14 17:46:19 -0400

    inc z

--- src/components/Paper/Paper.js
+++ src/components/Paper/Paper.js
@@ -2,7 +2,7 @@ import React from 'react'
 import classNames from 'classnames'
 import { clsElevation } from '../../constants/ui'
 // import './Paper.css'
-const Paper = ({ children, className, style, transition, z = 5 }) =>
+const Paper = ({ children, className, style, transition, z = 4 }) =>
   <div
     className={classNames(className, { [`${clsElevation}-transition`]: transition }, `${clsElevation}--z${z}`)}
     style={style}>
